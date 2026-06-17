9464815da6205abf63b3153add9b5272ce79866f
art-bot5000 <pete@artbot5000.com>
2026-06-17 15:31:58 +0100

    sw 6.36 further fixes

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Beast Mode // Service Worker
 // v4.1 — message-based update flow
 
-const CACHE_NAME = 'beast-mode-v6.35'
+const CACHE_NAME = 'beast-mode-v6.36'
 const CACHE_URLS = [
   './',
   './index.html',
