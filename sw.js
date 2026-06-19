1304ed167946d9b079a9dfd22fba257ea0affc9b
art-bot5000 <pete@artbot5000.com>
2026-06-19 15:18:49 +0100

    sw 6.67 gen mode frontend

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Beast Mode // Service Worker
 // v4.2 — message-based update flow
 
-const CACHE_NAME = 'beast-mode-v6.66'
+const CACHE_NAME = 'beast-mode-v6.67'
 const CACHE_URLS = [
   './',
   './index.html',
